2d336b1049ea9dcf4b1cb9413f8fbd0390307778
Will White <willdotwhiteis@gmail.com>
2021-04-29 20:56:55 +0100

    Add dummy RoleId values to mock Teams data

--- app/src/main/kotlin/team/finder/api/teams/Team.kt
+++ app/src/main/kotlin/team/finder/api/teams/Team.kt
@@ -1,5 +1,5 @@
 package team.finder.api.teams
 
-data class Team(val id: Int, val name: String) {
+data class Team(val id: Int, val name: String, val roleId: Int) {
     val author: String = "Some nerd"
 }
