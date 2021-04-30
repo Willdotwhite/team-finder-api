d9baa0e2a2d4a00eab7bad13a33dff658e33c3d3
awildbrysen <9168041+awildbrysen@users.noreply.github.com>
2021-04-30 15:03:38 +0200

    Add update for teams
    Added update (PUT) for team that changes the name of the team, this should be expanded but this is more as a starting point

--- app/src/main/kotlin/team/finder/api/teams/TeamsService.kt
+++ app/src/main/kotlin/team/finder/api/teams/TeamsService.kt
@@ -7,4 +7,13 @@ class TeamsService(val repository: TeamsRepository) {
     fun createTeam(team: Team) = repository.save(team)
     fun getTeams(): MutableIterable<Team> = repository.findAll()
     fun getTeamById(id: Long) = repository.findById(id)
+    fun updateTeam(id: Long, name: String): Team? {
+        val maybeTeam = this.getTeamById(id)
+        if (!maybeTeam.isPresent) {
+            return null
+        }
+        val team = maybeTeam.get()
+        team.name = name
+        return repository.save(team)
+    }
 }
