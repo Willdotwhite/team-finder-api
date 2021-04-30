ca324995490b34427b9cdaab1198cef5758d94b2
WillDotWhite <willdotwhiteis@gmail.com>
2021-04-30 18:20:49 +0100

    Split DB profiles into groups for MySQL usage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 
-    runtime("com.h2database:h2")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("mysql:mysql-connector-java")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
