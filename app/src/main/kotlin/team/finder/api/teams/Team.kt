a88b3a051740fd0d1a291e5e2ac5e9c9cbb39ddf
WillDotWhite <willdotwhiteis@gmail.com>
2021-04-30 08:23:45 +0100

    Improve naming of aggregate role data, and add dummy CreatedAt data for funsies

--- app/src/main/kotlin/team/finder/api/teams/Team.kt
+++ app/src/main/kotlin/team/finder/api/teams/Team.kt
@@ -1,5 +1,9 @@
 package team.finder.api.teams
 
-data class Team(val id: Int, val name: String, val roleId: Int) {
-    val author: String = "Some nerd"
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+data class Team(val id: Int, val name: String, val skillsetMask: Int) {
+    val author: String = "DiscordName#1234"
+    val createdAt: String = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
 }
