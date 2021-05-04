e7044a3f54fffcfa0ce60997ed1e108090d4d7e2
Francois Boivin <boivin.92@gmail.com>
2021-05-04 06:59:04 -0400

    Added basic validation to TeamDto

--- app/src/main/kotlin/team/finder/api/teams/TeamDto.kt
+++ app/src/main/kotlin/team/finder/api/teams/TeamDto.kt
@@ -1,7 +1,18 @@
 package team.finder.api.teams
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+
 class TeamDto(
+    @NotNull
+    @Pattern(regexp="^.{3,32}#[0-9]{4}\$",message="username must follow Discord Username guidelines") //3-32 of any chars followed by # followed by exactly 4 numbers
     var author: String,
+
+    @NotNull
+    @Max(140)
     var description: String,
+
+    @Max(1048575)
     var skillsetMask: Int,
 )
--- app/src/main/kotlin/team/finder/api/teams/TeamsController.kt
+++ app/src/main/kotlin/team/finder/api/teams/TeamsController.kt
@@ -3,6 +3,7 @@ package team.finder.api.teams
 import org.springframework.web.bind.annotation.*
 import java.util.*
 import javax.servlet.http.HttpServletResponse
+import javax.validation.Valid
 
 @RestController
 class TeamsController(val service: TeamsService) {
@@ -11,14 +12,14 @@ class TeamsController(val service: TeamsService) {
     fun index(@RequestParam skillsetMask: Int?) : List<Team> = service.getTeams(skillsetMask)
 
     @PostMapping("/teams")
-    fun add(@RequestBody teamDto: TeamDto) = service.createTeam(Team.fromDto(teamDto))
+    fun add(@Valid @RequestBody teamDto: TeamDto) = service.createTeam(Team.fromDto(teamDto))
 
     @GetMapping("/teams/{id}")
     fun view(@PathVariable id: Long) : Optional<Team> = service.getTeamById(id)
 
     // TODO: Only changed fields
     @PutMapping("/teams/{id}")
-    fun update(@PathVariable id: Long, @RequestBody teamDto: TeamDto) = service.updateTeam(id, teamDto.author, teamDto.description, teamDto.skillsetMask)
+    fun update(@PathVariable id: Long, @Valid @RequestBody teamDto: TeamDto) = service.updateTeam(id, teamDto.author, teamDto.description, teamDto.skillsetMask)
 
     @DeleteMapping("/teams/{id}")
     fun delete(@PathVariable id: Long) : Team? = service.deleteTeam(id)
