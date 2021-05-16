9d48e04f168d219514a314f437ce7ab5cfe914aa
Dallon Feldner <dallon.feldner@gmail.com>
2021-05-15 21:13:19 -0500

    Make it verify JWTs

--- app/src/main/kotlin/team/finder/api/auth/JwtVerifierConfiguration.kt
+++ app/src/main/kotlin/team/finder/api/auth/JwtVerifierConfiguration.kt
@@ -2,22 +2,12 @@ package team.finder.api.auth
 
 import com.nimbusds.jose.JWSVerifier
 import com.nimbusds.jose.crypto.MACVerifier
-import com.nimbusds.jose.jwk.JWK
-import com.nimbusds.jose.jwk.JWKSet
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet
-import com.nimbusds.jose.jwk.source.JWKSource
-import com.nimbusds.jose.proc.SecurityContext
-import com.nimbusds.jwt.JWTClaimsSet
-import com.nimbusds.jwt.SignedJWT
-import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier
-import com.nimbusds.jwt.proc.DefaultJWTProcessor
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import team.finder.api.system.RuntimeConfig
 
-@Component
-class JwtVerifier(config: RuntimeConfig) {
-    val verifier: JWSVerifier = MACVerifier(config.JwtSecret)
-
-    fun verify(jwt: SignedJWT): Boolean = jwt.verify(verifier)
+@Configuration
+class JwtVerifierConfiguration(val config: RuntimeConfig) {
+    @Bean
+    fun jwtVerifier(): JWSVerifier = MACVerifier(config.JwtSecret)
 }
