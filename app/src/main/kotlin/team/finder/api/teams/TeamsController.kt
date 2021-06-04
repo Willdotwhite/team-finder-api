fffaeac76d132cefaa59f2b37e430e9a26b81ff2
WillDotWhite <willdotwhiteis@gmail.com>
2021-06-04 21:33:53 +0100

    Improve filtering to catch nonsense input

--- app/src/main/kotlin/team/finder/api/teams/TeamsController.kt
+++ app/src/main/kotlin/team/finder/api/teams/TeamsController.kt
@@ -112,8 +112,10 @@ class TeamsController(
         return input
             .toLowerCase()                          // Standardise casing for cache
             .replace(Regex("[=;,'\"]"), " ")        // Remove some SQL-specific characters for crude sanitisation
-            .replace(Regex("/[^a-z0-9_]/g"), "")    // Remove all unwanted search characters (RIP emoji)
+            .replace(Regex("[^a-z0-9_ ]"), "")      // Remove all unwanted search characters (RIP emoji)
             .split(" ")                             // Break from Spring @RequestParam formatting for sorting
+            .distinct()                             // Filter out duplicate entries
+            .filterNot { it == "" }                  // Filter out whitespace
             .sortedBy { it }                        // Sort terms alphabetically
             .joinToString("-")                      // Return to single string for cache entry
     }
